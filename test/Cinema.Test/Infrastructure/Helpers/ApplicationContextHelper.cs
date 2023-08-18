9d2fcff0b55988e97f93f1be85c885ef13243e2c
BritvA <apan1998@yandex.ru>
2023-08-18 16:21:25 +0300

    Реализовано тестирование сервисов

---
+++ test/Cinema.Test/Infrastructure/Helpers/ApplicationContextHelper.cs
@@ -0,0 +1,38 @@
+﻿using Cinema.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Test.Infrastructure.Helpers;
+
+internal class ApplicationContextHelper
+{
+    public ApplicationContext Context { get; private set; }
+    public DbContextOptions<ApplicationContext> Options { get; private set; }
+
+    public ApplicationContextHelper()
+    {
+        //builder.UseInMemoryDatabase("cinema_test_db")
+        //    .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+        var builder = new DbContextOptionsBuilder<ApplicationContext>();
+        builder.UseNpgsql("Host=localhost;Port=5432;Database=cinemadb_test;User Id=postgres;Password=pgpassword;");
+        //builder.LogTo(message => System.Diagnostics.Debug.WriteLine(message));
+
+        Options = builder.Options;
+        Context = new ApplicationContext(Options);
+
+        if (Context.Database.EnsureCreated())
+        {
+            Context.Genres.AddRange(DataHelper.GenreList);
+            Context.Movies.AddRange(DataHelper.MovieList);
+            Context.SaveChanges();
+
+            Context.MovieGenres.AddRange(DataHelper.MovieGenreList);
+            Context.SaveChanges();
+
+            Context.Schedules.AddRange(DataHelper.ScheduleList);
+            Context.SaveChanges();
+
+            Context.Orders.AddRange(DataHelper.OrderList);
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ test/Cinema.Test/Infrastructure/Supports/DatabaseFixture.cs
@@ -0,0 +1,49 @@
+﻿using Cinema.BLL.Services.Additional;
+using Cinema.Test.Infrastructure.Helpers;
+using System.Reflection;
+
+namespace Cinema.Test.Infrastructure.Supports;
+
+public class DatabaseFixture : IDisposable
+{
+    private readonly ApplicationContextHelper _helper;
+
+    public string? AppDir { get; private set; }
+
+    public DatabaseFixture()
+    {
+        _helper = new ApplicationContextHelper();
+
+        AppDir = Path.GetDirectoryName(
+            Assembly.GetExecutingAssembly().Location);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    private bool disposed = false;
+    public virtual void Dispose(bool disposing)
+    {
+
+        if (disposed) return;
+        if (disposing)
+        {
+            _helper.Context.Database.EnsureDeleted();
+            _helper.Context.Dispose();
+
+            if (AppDir is not null)
+            {
+                FileService.ClearAll(Path.Combine(AppDir, "ImageDb"));
+            }
+        }
+        disposed = true;
+    }
+
+    ~DatabaseFixture()
+    {
+        Dispose(false);
+    }
+}
